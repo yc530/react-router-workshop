4cde24ec669e526b950511c1a179407942b306d1
CourtneyATX <courtney.mcguire.tx@gmail.com>
2023-10-19 16:15:43 -0500

    removed :shades

--- reactRouter/src/App.jsx
+++ reactRouter/src/App.jsx
@@ -29,17 +29,11 @@ function App() {
           <Route path="/" element={<Home />}></Route>
           {/* <Route path="/shades" element={<AllShades />}></Route> */}
           <Route path="/red" element={<Red />}> </Route>
-          <Route path="/red/:shades" element={<Red />}></Route>
           <Route path="/orange" element={<Orange />}> </Route>
-          <Route path="/orange/:shades" element={<Orange />}></Route>
           <Route path="/yellow" element={<Yellow />}> </Route>
-          <Route path="/yellow/:shades" element={<Yellow />}></Route>
           <Route path="/green" element={<Green />}> </Route>
-          <Route path="/green/:shades" element={<Green />}></Route>
           <Route path="/blue" element={<Blue />}> </Route>
-          <Route path="/blue/:shades" element={<Blue />}></Route>
           <Route path="/violet" element={<Violet />}> </Route>
-          <Route path="/violet/:shades" element={<Violet />}></Route>
         </Routes>
       </div>
       <footer className="footer">
