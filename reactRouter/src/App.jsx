4892563029706239b35af8759322dc6de002ecb1
CourtneyATX <courtney.mcguire.tx@gmail.com>
2023-10-19 12:35:57 -0500

    add home.jsx with code

--- reactRouter/src/App.jsx
+++ reactRouter/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
 import Blue from './components/blue';
 import Red from './components/red';
+import Home from './components/home';
 
 function App() {
 
@@ -10,11 +11,13 @@ function App() {
     <div id="container">
       <h1>  </h1>
       <div id="navbar">
+        <Link to="/">Home</Link>
         <Link to="blue">Blue</Link>
         <Link to="red">Red</Link>
       </div>
       <div id="main-section">
         <Routes>
+          <Route path="/" element={<Home />}></Route>
           <Route path="/blue" element={<Blue />}></Route>
           <Route path="/red" element={<Red />}></Route>
         </Routes>
