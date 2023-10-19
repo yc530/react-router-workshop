99a9fc21e061184e0470b9abe2255488e691a8f7
CourtneyATX <courtney.mcguire.tx@gmail.com>
2023-10-19 12:20:01 -0500

    red/blue jsx and links added

--- reactRouter/src/App.jsx
+++ reactRouter/src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
 
   return (
     <>
     <div id="container">
-      <h1> Hi </h1>
+      <h1>  </h1>
       <div id="navbar">
-        {/* Navigation Here */}
+        <Link to="blue">Blue</Link>
+        <Link to="red">Red</Link>
       </div>
       <div id="main-section">
         <Routes>
