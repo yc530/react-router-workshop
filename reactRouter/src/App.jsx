b58b932ef5fcebd9218fcf6af66bd905d695c6bf
CourtneyATX <courtney.mcguire.tx@gmail.com>
2023-10-19 13:48:21 -0500

    added rainbow css, routes, and links

--- reactRouter/src/App.jsx
+++ reactRouter/src/App.jsx
@@ -1,7 +1,11 @@
-import './App.css';
+import './index.css';
 import { Routes, Route, Link } from "react-router-dom";
-import Blue from './components/blue';
 import Red from './components/red';
+import Orange from './components/orange';
+import Yellow from './components/yellow';
+import Green from './components/green';
+import Blue from './components/blue';
+import Violet from './components/violet';
 import Home from './components/home';
 
 function App() {
@@ -9,17 +13,24 @@ function App() {
   return (
     <>
     <div id="container">
-      <h1>  </h1>
       <div id="navbar">
         <Link to="/">Home</Link>
-        <Link to="blue">Blue</Link>
         <Link to="red">Red</Link>
+        <Link to="orange">Orange</Link>
+        <Link to="yellow">Yellow</Link>
+        <Link to="green">Green</Link>
+        <Link to="blue">Blue</Link>
+        <Link to="violet">Violet</Link>
       </div>
       <div id="main-section">
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/blue" element={<Blue />}></Route>
           <Route path="/red" element={<Red />}></Route>
+          <Route path="/orange" element={<Orange />}></Route>
+          <Route path="/yellow" element={<Yellow />}></Route>
+          <Route path="/green" element={<Green />}></Route>
+          <Route path="/blue" element={<Blue />}></Route>
+          <Route path="/violet" element={<Violet />}></Route>
         </Routes>
       </div>
     </div>
